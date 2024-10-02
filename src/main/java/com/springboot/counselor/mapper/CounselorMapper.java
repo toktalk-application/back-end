b7e44340003393aaefe87d7873519398268abbb9
whgkswo <gjuk7238@gmail.com>
2024-10-02 15:27:34 +0900

    feat ReservationTimeValidation(#19)

--- src/main/java/com/springboot/counselor/mapper/CounselorMapper.java
+++ src/main/java/com/springboot/counselor/mapper/CounselorMapper.java
@@ -1,13 +1,52 @@
 package com.springboot.counselor.mapper;
 
+import com.springboot.counselor.available_date.AvailableDate;
+import com.springboot.counselor.dto.AvailableDateDto;
+import com.springboot.counselor.dto.AvailableTimeDto;
 import com.springboot.counselor.dto.CounselorDto;
 import com.springboot.counselor.entity.Counselor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = AvailableDateMapper.class)
 public interface CounselorMapper {
     Counselor counselorPostDtoToCounselor(CounselorDto.Post postDto);
     Counselor counselorPatchDtoToCounselor(CounselorDto.Patch patchDto);
-    CounselorDto.Response counselorToCounselorResponseDto(Counselor counselor);
+    default CounselorDto.Response counselorToCounselorResponseDto(Counselor counselor){
+        List<AvailableDate> availableDateList = new ArrayList<>(counselor.getAvailableDates().values());
+
+        return new CounselorDto.Response(
+                counselor.getCounselorId(),
+                counselor.getBirth(),
+                counselor.getGender(),
+                counselor.getCounselorStatus(),
+                counselor.getName(),
+                counselor.getUserId(),
+                counselor.getCompany(),
+                availableDateList.stream()
+                        .map(date -> new AvailableDateDto(
+                                date.getAvailableDateId(),
+                                date.getDate(),
+                                date.getAvailableTimes().stream()
+                                        .map(time -> new AvailableTimeDto(
+                                                time.getAvailableTimeId(),
+                                                time.getStartTime(),
+                                                time.getEndTime(),
+                                                time.getReservation() != null
+                                        )).collect(Collectors.toList())
+                        ))
+                        .collect(Collectors.toList()),
+                counselor.getChatPrice(),
+                counselor.getCallPrice(),
+                counselor.getCreatedAt(),
+                counselor.getModifiedAt()
+        );
+    };
+    /*@Mapping(source = "availableDates", target = "availableDates")
+    CounselorDto.Response counselorToCounselorResponseDto(Counselor counselor);*/
 }
