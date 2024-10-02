6b5dbc676612c05a1d87149b97f1a2260c04ac17
JinHyeok Hwang <dujin132@gmail.com>
2024-10-02 10:22:16 +0900

    feat:#12 RegistrationController 코드 개선

--- src/main/java/com/springboot/registration/controller/RegistrationController.java
+++ src/main/java/com/springboot/registration/controller/RegistrationController.java
@@ -3,10 +3,7 @@ package com.springboot.registration.controller;
 import com.springboot.registration.service.CodeAuthService;
 import com.springboot.registration.service.RegistrationAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
