084ef0e30bbe01d6a74fb56b699a39f366b697cb
JinHyeok Hwang <dujin132@gmail.com>
2024-10-11 10:42:11 +0900

    feat:registration API 리펙토링

--- src/main/java/com/springboot/registration/controller/RegistrationController.java
+++ src/main/java/com/springboot/registration/controller/RegistrationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/identity")
+@RequestMapping("/identity")
 public class RegistrationController {
     @Autowired
     private CodeAuthService authService;
